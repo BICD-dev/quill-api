8939258849e36406a0dd60ccbc3ce16957ead7ca
Bright Iheawgam <bright06iheagwam@gmail.com>
2025-10-01 04:21:20 +0100

    database configuration

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -2,5 +2,27 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async(): Promise<void> =>{
-    
+    try{
+        const mongoUrl = process.env.MONGO_URI;
+
+        if(!mongoUrl){
+             throw new Error('MONGO_URI is not defined in the environment variables');   
+        } 
+        await mongoose.connect(mongoUrl);
+
+        process.on('SIGINT', async() =>{
+            await mongoose.connection.close();
+            process.exit(0);
+        });
+    } catch(error){
+        throw new Error(`Failed to connect to mongoDB: ${error}`);
+    }
+}
+
+export const disconnectDB = async(): Promise<void> =>{
+    try{
+        await mongoose.connection.close();
+    } catch(error){
+        throw new Error(`Failed to disconnect from mongoDB: ${error}`);
+    }
 }
